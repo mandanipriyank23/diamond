049ff66f9e49dfe819e6c763baad152c1b0da86a
mandanipriyank23 <priyank.emedstore@gmail.com>
2025-07-17 14:47:49 +0530

    partial-front(folder under file)
    follow instraction can run now page

---
+++ menu.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title>Home</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div class="header">
+        <h1>Maruti Jwelers💎<h1>
+</div></br>
+    <div class="menu text-left">
+            <div class="wrapper">
+                <ul>
+                    <li><a href="index.php">HOME</a></li>
+                    <li><a href="manage-service.php">SERVICE</a></li>
+                    <li><a href="about-us.php">ABOUT-US</a></li>
+                    <li><a href="contact-us.php">CONTACT-US</a></li></br>
+                    <li><a href="logout.php" class="logout-btn">LOGOUT</a></li>
+                    
+                </ul>
+            </div>   
+      </div> 
+                   
+</body>
+</html>
